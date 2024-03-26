cd11c3a88a49e8c0b4b7c325ddedb26135833259
@mohamed32145 <mohamdsliman41@gmail.com>
2024-03-26 20:46:41 +0200

    building the project

---
+++ exceptions/exception.py
@@ -0,0 +1,11 @@
+def call():
+    print("do something")
+
+
+class Exception1:
+
+    def __init__(self):
+        super()
+
+    def do():
+        print(f"inner function inside the class with this number ")
---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+from exceptions import *
+from exceptions import exception
+from exceptions.exception import Exception1
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+p1 = exception
+
+
+
+p2 = Exception1()
+
+p2.do
+
+
+
+p1.call()
+
+
